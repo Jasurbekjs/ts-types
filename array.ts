1514476e195bfc106012efb75c20433b62f59794
Jasurbek <jasmirzaev@gmail.com>
2023-08-13 12:04:59 +0300

    array types

---
+++ array.ts
@@ -0,0 +1,10 @@
+let arr: string[]
+
+arr = ['1', '2']
+
+const numbers: ReadonlyArray<number> = [0,1,2]
+
+numbers.map(i=>i)
+
+type TArray = [number, string, null]
+const newArr: TArray = [1, 'asfd', null]
